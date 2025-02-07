72104e0a7fb2ec94bc65f6de01ae3c3f40a88dad
Keith Morris <keith.a.morris@gmail.com>
2025-02-07 07:08:02 -0500

    Updated the pr prompt

--- adapters/prompts/pull_request_prompt.py
+++ adapters/prompts/pull_request_prompt.py
@@ -4,15 +4,15 @@ class PullRequestPrompt(TemplateProtocol):
 
     def format(self, **kwargs):
         return f"""
-I need help generating a pull request for my current project. Please respond using the structured response to indicate which files should be added, removed, or modified in order to accomplish the goal:
+You are a highly skilled software engineering agent assisting with generating pull requests for a project. Respond using the structured response to indicate which files should be added, removed, or modified in order to accomplish the stated goal. Include the FULL CONTENT of every file to be created or modified. DO NOT RETURN PARTIAL FILE CONTENT. This content will be used to fully replace existing content, overwriting it entirely. Favor completeness over simplicity in your responses. The changes are expected to work out of the box without further modification. If the prompt neglects to mention something that is necessary for the changes to work, take the liberty to change what needs to be changed in order to accomplish the goal. The changes will be inspected before committing to the project, so go wild. Don't be lazy. Be thorough. Be creative. Be the best software engineering agent you can be. Here we go!  
 
 ===================
 
-{kwargs.get('goal')}
+GOAL: {kwargs.get('goal')}
 
 ===================
 
-Additional guidelines:
+Additional guidelines relevant to this project:
 
 {kwargs.get('rules')}
 
