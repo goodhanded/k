aa23ec3cf9957bd6e111f9e6a7d03226ded3191a
Keith Morris <keith.a.morris@gmail.com>
2025-02-14 09:23:16 -0500

    Added wiki template

--- infrastructure/init/__init__.py
+++ infrastructure/init/__init__.py
@@ -2,3 +2,4 @@ from .templates.dotnet import DotNetTemplate
 from .templates.nextjs import NextJSTemplate
 from .templates.nodejs import NodeJSTemplate
 from .templates.python import PythonTemplate
+from .templates.wiki import WikiTemplate
---
+++ infrastructure/init/templates/wiki.py
@@ -0,0 +1,18 @@
+from application.init import KTemplateProtocol
+
+class WikiTemplate(KTemplateProtocol):
+    def get_excludes(self) -> str:
+        return (
+            ".git\n"
+        )
+
+    def get_includes(self) -> str:
+        return (
+            "*.md\n"
+            ".order\n"
+        )
+
+    def get_rules(self) -> str:
+        return (
+            "- Pay attention to existing style conventions and try to keep it consistent.\n"
+        )
